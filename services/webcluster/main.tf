51dbd1786f7688d837f07a265dca800444bd32b3
Chris Hudson <chudson@worldremit.com>
2019-11-20 09:28:22 +0000

    last go

--- services/webcluster/main.tf
+++ services/webcluster/main.tf
@@ -51,8 +51,8 @@ resource "aws_autoscaling_group" "example" {
   dynamic "tag" {
     for_each = var.custom_tags
     content {
-      key = custom_tag.key
-      value = custom_tag.value
+      key = tag.key
+      value = tag.value
       propagate_at_launch = true
     }
   }
